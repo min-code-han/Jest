9012f149ab39e1f092c54234e5547e3ff56b9f73
mina <yegrinaaa@gmail.com>
2023-01-02 14:46:04 +0900

    mock 함수를 이용한 테스트

---
+++ src/mock/check/test/check.test.js
@@ -0,0 +1,29 @@
+const check = require("../check")
+
+describe("check",()=>{
+ let onSuccess;
+ let onFail;
+
+ beforeEach(()=>{
+     onSuccess = jest.fn()
+     onFail = jest.fn()
+ })
+
+    it("should call onSuccess when predicate is true", ()=>{
+        check(()=>true,onSuccess, onFail)
+
+        // expect(onSuccess.mock.calls.length).toBe(1)
+        expect(onSuccess).toHaveBeenCalledTimes(1)
+        // expect(onSuccess.mock.calls[0][0]).toBe("yes")
+        expect(onSuccess).toHaveBeenLastCalledWith("yes")
+        expect(onFail.mock.calls.length).toBe(0)
+    })
+
+    it("should call onFail when predicate is false", ()=>{
+        check(()=>false,onSuccess, onFail)
+
+        expect(onFail).toHaveBeenCalledTimes(1)
+        expect(onFail).toHaveBeenLastCalledWith("no")
+        expect(onSuccess.mock.calls.length).toBe(0)
+    })
+})
